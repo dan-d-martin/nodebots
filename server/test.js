e051daf601c79b95c03ea41b9d7898911e5c919a
Dan Martin <dan@dan-martin.net>
2014-05-02 14:20:16 +0100

    test robot now walks round perimeter

--- server/test.js
+++ server/test.js
@@ -3,11 +3,6 @@
 	i = 0;
 	dir = 0;
 	socket.on('turn', function() {
-		if(i++%5) {
-			dir++;
-			if(dir > 3)
-				dir = 0;
-		}
 		if(dir == 0)
 			socket.emit('right');
 		else if (dir == 1)
@@ -16,4 +11,8 @@
 			socket.emit('left');
 		else if (dir == 3)
 			socket.emit('up');	
-	});
+		if(++i % 4 == 0) {
+			dir++;
+			if(dir > 3)
+				dir = 0;
+	}});
